998df1814a2ff0a76e619332bf231da055d5d0c9
Alex <lesha.rudiko@gmail.com>
2024-05-09 13:04:08 +0300

    update

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,21 +4,11 @@
       <div class="row">
         <div class="col-lg-6 offset-lg-3">
           <ul class="footer d-flex flex-wrap" @click="scrolledTop">
-            <li class="footer__item">
-              <router-link to="/">
-                <img src="@/assets/logo/Logo_black.svg" alt="logo" />
+            <li v-for="(link, index) of arrLinks" :class="link.classLi">
+              <router-link :to="link.path">
+                <img v-if="index === 0" :src="require(`@/assets/logo/${link.img}`)" alt="icon">
+                <p v-else>{{link.text}}</p>
               </router-link>
-            </li>
-            <li class="footer__item">
-              <router-link :to="{ name: 'OurCoffe' }"> Our coffee </router-link>
-            </li>
-            <li class="footer__item">
-              <router-link :to="{ name: 'Goods' }">
-                For your pleasure
-              </router-link>
-            </li>
-            <li class="footer__item">
-              <router-link :to="{ name: 'Contact' }"> Contact us </router-link>
             </li>
           </ul>
         </div>
@@ -35,12 +25,45 @@
 <script>
 
 export default{
-    setup(){
-        const scrolledTop = () => {
-           window.scrollTo({top: 0, behavior: 'smooth'})
-        }
+  data(){
+    let arrLinks = [
+      {
+        classLi: 'footer__item',
+        path: '/',
+        img: 'Logo_black.svg',
+      },
+      {
+        classLi: 'footer__item',
+        path: {name: 'OurCoffe'},
+        text: 'Our coffee'
+      },
+      {
+        classLi: 'footer__item',
+        path: {name: 'Goods'},
+        text: 'For your pleasure',
+      },
+      {
+        classLi: 'footer__item',
+        path: {name: 'Contact'},
+        text: 'Contact us',
+      }
+    ]
 
-        return {scrolledTop}
+    return{
+      arrLinks
     }
+  },
+
+  methods:{
+    scrolledTop() {
+      window.scrollTo({top: 0, behavior: 'smooth'})
+    }
+  },
 }
 </script>
+
+<style scoped>
+  .footer__item p{
+    margin-bottom: 0;
+  }
+</style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,28 +1,55 @@
 <template>
   <header>
     <ul
-      class="header d-flex justify-content-center justify-content-md-start flex-wrap"
+        class="header d-flex justify-content-center justify-content-md-start flex-wrap"
     >
-      <li class="header__item">
-        <router-link to="/">
-          <img src="@/assets/logo/Logo.svg" alt="logo" />
-        </router-link>
-      </li>
-      <li class="header__item">
-        <router-link :to="{name: 'OurCoffe'}">
-          Our coffee
-        </router-link>
-      </li>
-      <li class="header__item">
-        <router-link :to="{name: 'Goods'}">
-          For your pleasure
-        </router-link>
-      </li>
-      <li class="header__item">
-        <router-link :to="{name: 'Contact'}">
-          Contact us
+      <li v-for="(link, index) of arrLinks" :class="link.classLi">
+        <router-link :to="link.path">
+          <img v-if="index === 0" :src="require(`@/assets/logo/${link.img}`)" alt="icon">
+          <p v-else>{{link.text}}</p>
         </router-link>
       </li>
     </ul>
   </header>
 </template>
+
+<script>
+
+export default {
+  data(){
+    let arrLinks = [
+      {
+        classLi: 'header__item',
+        path: '/',
+        img: 'Logo.svg',
+      },
+      {
+        classLi: 'header__item',
+        path: {name: 'OurCoffe'},
+        text: 'Our coffee'
+      },
+      {
+        classLi: 'header__item',
+        path: {name: 'Goods'},
+        text: 'For your pleasure',
+      },
+      {
+        classLi: 'header__item',
+        path: {name: 'Contact'},
+        text: 'Contact us',
+      }
+    ]
+
+    return{
+      arrLinks
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+  .header__item p{
+    margin-bottom: 0;
+  }
+</style>
